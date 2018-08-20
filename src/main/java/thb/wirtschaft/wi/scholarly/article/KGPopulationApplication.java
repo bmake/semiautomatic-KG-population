de041a14e76cf6db513149a86ef7936843fcad57
Wenxin Hu <hu@th-brandenburg.de>
2018-08-20 21:31:29 +0800

    Codes upload

---
+++ src/main/java/thb/wirtschaft/wi/scholarly/article/KGPopulationApplication.java
@@ -0,0 +1,9 @@
+package thb.wirtschaft.wi.scholarly.article;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("KG Population App")
+public class KGPopulationApplication extends ServletProcessApplication {
+	// empty implementation
+}
